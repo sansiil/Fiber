abab5e5bc0507937ac7d1e01f0ee76ac43ba6611
sansiil <sahanshiludash@gmail.com>
2023-01-31 20:19:50 +0545

    js for ham

---
+++ fiber.js
@@ -0,0 +1,9 @@
+function navbar(){
+    const navs=document.getElementById('topmid');
+    if(navs.style.display ==='block' || navs.style.display ==="undefined"){
+        navs.style.display='none';
+    }
+    else{
+        navs.style.display='block';
+        }
+}
